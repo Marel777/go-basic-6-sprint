60b63ac2e3083fd8e6459621ca977d87b3c53b15
Dmitry Savelev <eventlogs@yandex.ru>
2025-12-07 19:26:06 +0300

    first-try

--- internal/server/server.go
+++ internal/server/server.go
@@ -1,1 +1,50 @@
 package server
+
+import (
+	"log"
+	"net/http"
+	"time"
+
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/internal/handlers"
+)
+
+const (
+	serverAddr         = ":8080"          // Порт сервера
+	serverReadTimeout  = 5 * time.Second  // Таймаут для чтения
+	serverWriteTimeout = 10 * time.Second // Таймаут для записи
+	serverIdleTimeout  = 15 * time.Second // Таймаут ожидания следующего запроса
+)
+
+// Структура с полями для логгера и http сервера
+type MorseDecoder struct {
+	Logger *log.Logger
+	Server *http.Server
+}
+
+// Функция создает mux и возвращает экземпляр сервера
+func NewServer(logger *log.Logger) MorseDecoder {
+	var md MorseDecoder
+	mux := http.NewServeMux()
+	registerHandlers(mux)                 // Зарегистрировали обработчики
+	md.Server = createServer(mux, logger) // Создали http
+	md.Logger = logger                    // Логгер
+	return md
+}
+
+// Функция регистрации Handlers
+func registerHandlers(mux *http.ServeMux) {
+	mux.HandleFunc(`/`, handlers.MainHandler)
+	mux.HandleFunc(`/upload`, handlers.UploadHandler)
+}
+
+// Создание http сервера
+func createServer(mux *http.ServeMux, logger *log.Logger) *http.Server {
+	var server http.Server
+	server.Addr = serverAddr
+	server.Handler = mux
+	server.ErrorLog = logger
+	server.ReadTimeout = serverReadTimeout
+	server.WriteTimeout = serverWriteTimeout
+	server.IdleTimeout = serverIdleTimeout
+	return &server
+}
--- internal/service/service.go
+++ internal/service/service.go
@@ -1,1 +1,31 @@
 package service
+
+import (
+	"errors"
+
+	"github.com/Yandex-Practicum/go1fl-sprint6-final/pkg/morse"
+)
+
+// Если строка содержит любой символ кроме .-
+// вернуть true
+func сontainsSymbol(s string) bool {
+	for _, r := range s {
+		if r != '.' && r != '-' && r != ' ' {
+			return true
+		}
+	}
+	return false
+}
+
+func Convert(s string) (string, error) {
+	var result string
+	if сontainsSymbol(s) {
+		result = morse.ToMorse(s)
+	} else {
+		result = morse.ToText(s)
+	}
+	if len(result) == 0 {
+		return result, errors.New("cannot convert string")
+	}
+	return result, nil
+}
